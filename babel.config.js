6b9115af3a6e26b049df2c8c0fac62b28b67e957
Julian tamayo <julian-9419@hotmail.com>
2025-05-15 20:19:20 -0500

    config alias and package

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@navigation': './src/navigation',
+            '@shared': './src/shared',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        }
+      ]
+    ]
+  };
+};
