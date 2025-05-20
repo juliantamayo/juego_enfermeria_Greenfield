d8ea136c42f67c1f300ac4261d79093afcb8adf0
Julian tamayo <julian-9419@hotmail.com>
2025-05-19 20:17:05 -0500

    [US-7] Dictionary title page

--- src/components/cases/case1/Case1Menu.screen.tsx
+++ src/components/cases/case1/Case1Menu.screen.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Text, View, StyleSheet, ImageBackground } from "react-native";
 import Header from "@shared/components/header/header.component";
 import { useAppNavigation } from "@navigation/hooks/useAppNavigation";
+import { useScreenTitle } from "@shared/hooks/useScreenTitle";
 import { GlobalStyles } from "@styles-theme";
 
 const Case1MenuScreen = () => {
   const navigation = useAppNavigation();
+  useScreenTitle("commom.titlePage.Case1Menu");
 
   return (
     <ImageBackground source={require("@images/layout/background.png")} style={GlobalStyles.container}>
