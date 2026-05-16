86fe9bca4fba89f0d26955d81ba448280f30bb22
Julian tamayo <julian-9419@hotmail.com>
2026-05-16 11:10:30 -0500

    [US-19] Flow questions 4 scene 2 (case 1)

---
+++ src/components/cases/case1/scenes/scene2/data/questionFlows.ts
@@ -0,0 +1,13 @@
+import question1Flow from "./question1Flow";
+import question2Flow from "./question2Flow";
+import question3Flow from "./question3Flow";
+import question4Flow from "./question4Flow";
+
+const scene2QuestionFlows = {
+  ...question1Flow,
+  ...question2Flow,
+  ...question3Flow,
+  ...question4Flow,
+};
+
+export default scene2QuestionFlows;
