ab5584f9408876be46056e7a6fa89169537e8c14
Julian tamayo <julian-9419@hotmail.com>
2026-05-18 15:14:46 -0500

    [US-34] Flow area 3

---
+++ src/components/cases/case1/scenes/scene3/data/area4Flow.ts
@@ -0,0 +1,26 @@
+import type { Scene3AssessmentFlow } from "./assessmentFlow.types";
+
+const muscleStrengthImage = require("@images/CASO_1_SEÑOR_CJM_58_AÑOS_P4_fuerza_muscular.gif");
+const nurseThinkingImage = require("@images/enfermera_pensando.png");
+
+const area4Flow: Record<string, Scene3AssessmentFlow> = {
+  area4: {
+    dialog: [
+      { id: "0", speaker: "nurse", image: muscleStrengthImage },
+      { id: "1", speaker: "nurse", image: muscleStrengthImage },
+    ],
+    quiz: [
+      {
+        id: "0",
+        questionKey: "case1.scene3.assessmentDialogs.area4.quiz.0.question",
+        answers: [
+          { id: "1", textKey: "case1.scene3.assessmentDialogs.area4.quiz.0.answers.1", correct: true },
+          { id: "2", textKey: "case1.scene3.assessmentDialogs.area4.quiz.0.answers.2" },
+        ],
+      },
+    ],
+    followUpDialog: [{ id: "0", speaker: "nurse", image: nurseThinkingImage }],
+  },
+};
+
+export default area4Flow;
